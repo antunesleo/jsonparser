d1b0c9473aeb6bada2db1679963b9d1f592914bc
Léo Antunes <antunesleo4@gmail.com>
2026-05-04 21:43:16 -0300

    implement parsing null

--- parser.py
+++ parser.py
@@ -5,22 +5,43 @@ class ParsingError(Exception):
 def parsestr(json_str: str):
     stack = []
 
-    for char in json_str:
-        if char == " ":
-            continue
-        if char == "{":
-            if len(stack) > 0:
-                raise ParsingError("ha!")
+    first_char_index = 0
+    for index, char in enumerate(json_str):
+        if char != " ":
+            first_char_index = index
+            break
+
+    first_char = json_str[first_char_index]
+    if first_char == "n":
+        possible_null = []
+        for index in range(first_char_index, min(first_char_index+4, len(json_str))):
+            possible_null.append(json_str[index])
+        if "".join(possible_null) == "null":
+            return None
+        raise ParsingError("aha!")
+
+    if first_char == "{":
+        for index in range(first_char_index, len(json_str)):
+            char = json_str[index]
+
+            if char == " ":
+                continue
+
+            if char == "{":
+                if len(stack) > 0:
+                    raise ParsingError("ha!")
+                
+                stack.append(char)
             
-            stack.append(char)
-        
-        if char == "}":
-            if not stack:
-                raise ParsingError("ha!")
-        
-            stack.pop()
-        
-    if stack:
-        raise ParsingError("ha!")
+            if char == "}":
+                if not stack:
+                    raise ParsingError("ha!")
+            
+                stack.pop()
+            
+        if stack:
+            raise ParsingError("ha!")
     
-    return {}
+        return {}
+    
+    raise ParsingError("ha!")
--- test_parser.py
+++ test_parser.py
@@ -17,4 +17,20 @@ def test_parse_empty_object(json_str):
 )
 def test_parse_wrong_empty_object(wrong_json):
     with pytest.raises(ParsingError):
-        assert parsestr(wrong_json) == {}
+        parsestr(wrong_json)
+
+
+@pytest.mark.parametrize(
+    "json_str",
+    ["null", " null", "null ", " null "]
+)
+def test_parse_null(json_str):
+    assert parsestr(json_str) == None
+
+@pytest.mark.parametrize(
+    "wrong_json",
+    ["n ull", " nu ll", "NULL ", "none", "\{null\}"]
+)
+def test_parse_null(wrong_json):
+    with pytest.raises(ParsingError):
+        parsestr(wrong_json)
